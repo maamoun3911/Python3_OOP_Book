c69056c0cf914947d4f6f570ec0d5f9b4002881c
Maamoun <maamoun.haj.najeeb@gmail.com>
2023-05-31 11:32:52 +0300

    31/5/2023

--- Chapter6/python_data_structures.py
+++ Chapter6/python_data_structures.py
@@ -63,31 +63,108 @@ from collections import namedtuple
 
 #############################################################
 
-from collections import defaultdict
+# from collections import defaultdict
 
-def tuple_counter(dictionary, key, list_value) -> tuple[int, list]:
-    dictionary[key] = len(dictionary)+1, []
-    dictionary[key][1].append(list_value)
-    # return (num_items, [])
+# num_items = 0
+# def tuple_counter():
+#     global num_items
+#     num_items += 1
+#     return (num_items, [])
 
-d = defaultdict(tuple[int, list])
-tup_con = tuple_counter(d, "a", "hello")
-tup_con = tuple_counter(d, "b", ", world")
-tup_con = tuple_counter(d, "c", "!")
-tup_con = tuple_counter(d, "c", "?")
-
-print(d)
+# d = defaultdict(tuple_counter)
+# d['a'][1].append("hello")
+# d['b'][1].append('world')
+# d['b'][1].append("you")
+# print(d)
 
 
-num_items = 0
-def tuple_counter():
-    global num_items
-    num_items += 1
-    return (num_items, [])
-d = defaultdict(tuple_counter)
+#########################
+# I leave global variables because of it's problems and turn into classes, much easier
 
-d = defaultdict(tuple_counter)
-d['a'][1].append("hello")
-d['b'][1].append('world')
-d['b'][1].append("you")
-print(d)
+# from collections import defaultdict
+
+
+# class Counting:
+#     def __init__(self) -> None:
+#         self.counter = 0
+    
+#     def tuple_counter(self) -> tuple[int, list]:
+#         self.counter += 1
+#         return self.counter, []
+
+
+# tuple_counter = Counting().tuple_counter
+# defdict = defaultdict(tuple_counter)
+# # defdict = defaultdict(counter.tuple_counter())
+# # if we do this it will give error
+# # TypeError: first argument must be callable or None
+# defdict["a"][1].append("hello")
+# defdict['b'][1].append('world')
+# defdict['b'][1].append("you")
+# print(defdict)
+
+#######################################################
+# Lists
+#######
+# import string
+
+# CHARACTERS = list(string.ascii_letters)
+
+# def letter_frequencies(sentence):
+#     frequencies: list[tuple[str, int]] = [(c, 0) for c in CHARACTERS]
+#     for letter in sentence:
+#         index = CHARACTERS.index(letter)
+#         frequencies[index] = letter, frequencies[index][1]+1
+#     return frequencies
+# print(letter_frequencies("maamoun"))
+
+# class WeirdSortee:
+#     def __init__(self, string: str, number: int, sort_num: bool):
+#         self.string = string
+#         self.number = number
+#         self.sort_num = sort_num
+    
+#     def __lt__(self, object):
+#         if self.sort_num:
+#             return self.number < object.number
+#         return self.string < object.string
+    
+#     def __repr__(self):
+#         return f"{self.string}:{self.number}"
+
+
+# a = WeirdSortee('a', 4, True)
+# c = WeirdSortee('c', 3, True)
+# b = WeirdSortee('b', 2, True)
+# d = WeirdSortee('d', 1, True)
+# l = [a,b,c,d]
+
+# l.sort()
+# print(l)
+
+# x = [(1,'c'), (2,'a'), (3, 'b')]
+# x.sort()
+# print(x)
+# x.sort(key=lambda x: x[1])
+# print(x)
+
+###########################
+
+# class TooLong(Exception):
+#     pass
+
+# class ExtendedList(list):
+#     def append(self, __object: str) -> None:
+#         if len(__object) > 5:
+#             raise TooLong("Word must be less than 5 letters")
+#         return super().append(__object)
+    
+# lalista = ExtendedList()
+# lalista.append("Ahmad")
+# # lalista.append("Maamoun")
+# help(list.__setitem__)
+
+#############################
+
+# from collections import OrderedDict
+# help(OrderedDict)
